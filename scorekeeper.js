a37bf8aff43ccb952dfbeed72eb6073b90c46e43
Nikolas9896 <nhlynka@gmail.com>
2019-08-26 17:02:07 +0300

    add display and make p1Button and p2Button Event Listener

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,11 +1,17 @@
 var p1Button    =  document.querySelector("#player1");
 var p2Button    =  document.querySelector("#player2");
 var rButton     =  document.querySelector("#reset");
-var h2          =  document.querySelector("h2");
+var d1          =  document.querySelector("#p1Display");
+var d2          =  document.querySelector("#p2Display");
 var p1Score = 0;
 var p2Score = 0;
 
 p1Button.addEventListener("click", function(){
     p1Score++;
-    h2.textContent = p1Score;
+    d1.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function(){
+    p2Score++;
+    d2.textContent = p2Score;
 });
