907f4195184e954edb896d80beb563a7bf7403fe
Nikolas9896 <nhlynka@gmail.com>
2019-08-26 15:29:32 +0300

    add second solution with css class and toggle funtion

--- selector_ChangeBackground.js
+++ selector_ChangeBackground.js
@@ -1,5 +1,7 @@
 var isOrange=false;
 var button = document.querySelector("button");
+var buttonCss = document.getElementsByTagName("button")[1];
+//Solution with style tag
 button.addEventListener("click", function(){
 	if(isOrange){
 		
@@ -7,4 +9,8 @@ button.addEventListener("click", function(){
 	}
 	else {document.body.style.background = "orange";}
 	isOrange = !isOrange;
+});
+//Solution with specialBackground Class
+buttonCss.addEventListener("click", function(){
+	document.body.classList.toggle("specialBackground");
 });
