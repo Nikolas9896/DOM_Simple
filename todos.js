8a4e8e4fcb47d44a97376ccb52c29b2964768a43
Nikolas9896 <nhlynka@gmail.com>
2019-08-27 22:16:35 +0300

    add mouseout for 1st li to black

--- todos.js
+++ todos.js
@@ -1,8 +1,9 @@
-var firstLI = document.querySelector("li");
+var list = document.querySelectorAll("li");
 
 firstLI.addEventListener("mouseover", function(){
-
     this.style.color = "green";
+});
 
-
+firstLI.addEventListener("mouseout", function(){
+    this.style.color = "black";
 });
